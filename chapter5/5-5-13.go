7c4ca46a3311ebf652d969aa5ad7118f4d49f4ae
zhongyinghe <642188160@qq.com>
2017-08-03 17:28:50 +0800

    字段标签

---
+++ chapter5/5-5-13.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type user struct {
+	name string `昵称`
+	sex  byte   `性别`
+}
+
+func main() {
+	u := user{"Tom", 1}
+	v := reflect.ValueOf(u)
+
+	t := v.Type()
+
+	//t主类型， v主值
+	for i, n := 0, t.NumField(); i < n; i++ {
+		fmt.Printf("%s: %v\n", t.Field(i).Tag, v.Field(i))
+	}
+}
