aba0e0c722f22ab8c745b5267e15c4bbfb5b86fb
zhongyinghe <642188160@qq.com>
2017-08-15 16:57:10 +0800

    立即计算并复制执行参数

---
+++ chapter8/8-1-2.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"time"
+)
+
+var c int
+
+func counter() int {
+	c++
+	return c
+}
+
+func main() {
+	a := 100
+
+	go func(x, y int) {
+		time.Sleep(time.Second)
+		println("go:", x, y)
+	}(a, counter())
+
+	a += 100
+	println("main:", a, counter())
+
+	time.Sleep(time.Second * 3)
+}
