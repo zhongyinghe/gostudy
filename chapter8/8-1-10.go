b5c74b132d941d11ace7e8878ef7c662b31d2b31
zhongyinghe <642188160@qq.com>
2017-08-15 18:26:14 +0800

    Goexit终止当前任务，不可恢复

---
+++ chapter8/8-1-10.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"runtime"
+)
+
+func main() {
+	exit := make(chan struct{})
+
+	go func() {
+		defer close(exit)
+		defer println("a")
+
+		func() {
+			defer func() {
+				println("b", recover() == nil)
+			}()
+
+			func() {
+				println("c")
+				runtime.Goexit()
+				println("c done.")
+			}()
+
+			println("b done.")
+		}()
+
+		println("a done.")
+	}()
+
+	<-exit
+	println("main exit.")
+}
