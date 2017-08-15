5a0612ab690b82f476ea9308f63fe20d612eb0cf
zhongyinghe <642188160@qq.com>
2017-08-15 17:10:40 +0800

    使用sync.WaitGroup,等待多个任务结束，main才结束

---
+++ chapter8/8-1-4.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"sync"
+	"time"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+
+		go func(id int) {
+			defer wg.Done()
+
+			time.Sleep(time.Second)
+			println("goroutine", id, "done.")
+		}(i)
+	}
+
+	println("main...")
+
+	wg.Wait()
+
+	println("main exit.")
+}
