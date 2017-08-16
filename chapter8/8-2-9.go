809f0e5d3e92d1d28b3ba5a9924f7f84e3e16421
zhongyinghe <642188160@qq.com>
2017-08-16 18:18:50 +0800

    单向通道，明确发送者和接收者

---
+++ chapter8/8-2-9.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	c := make(chan int)
+	var send chan<- int = c
+	var recv <-chan int = c
+
+	go func() {
+		defer wg.Done()
+
+		for x := range recv {
+			println(x)
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		defer close(c)
+
+		for i := 0; i < 3; i++ {
+			send <- i
+		}
+	}()
+
+	wg.Wait()
+}
